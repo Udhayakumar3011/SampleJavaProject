1e4ee237be227e619cb201259e61e6ec3c551f4e
Udhay <Udhay@DESKTOP-PME6MC6>
2022-06-21 09:17:44 +0530

    Done!!!

---
+++ src/com/companyName/projectName/moduleName/SampleOne.java
@@ -0,0 +1,16 @@
+package com.companyName.projectName.moduleName;
+
+public class SampleOne {
+	public static void main(String[] kkk) {
+		System.out.println("Hello Vcentry");
+		System.err.println("Hello Vcentry");
+
+		// system-java.lang
+		// user can send and receive the input
+		// Out-Information-Without Error
+
+		// err-erro info-try-catch Handle
+		
+	}
+
+}
---
+++ src/com/companyName/projectName/moduleName/SampleTwo.java
@@ -0,0 +1,22 @@
+package com.companyName.projectName.moduleName;
+
+public class SampleTwo {
+	// int a = 10;
+
+	public static void main(String[] args) {
+		System.out.println("Hello Main Method");
+
+	}
+	// Without Main Method-We can't run.
+	// It always Search the Main Method First
+
+	public void module1() {
+		System.out.println("Hello V2");
+	}
+
+	public void Module2() {
+
+		System.out.println("Hello V1");
+	}
+
+}
